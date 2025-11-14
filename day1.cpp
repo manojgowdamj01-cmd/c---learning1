5c5e4fcc6a467314ff2eab38fe99cc4a1be0ffac
manojgowdamj01-cmd <manojgowdamj01@gmail.com>
2025-11-14 19:00:10 +0530

    u

--- day1.cpp
+++ day1.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
 int main(){
-    std::cout << "i like pizaa" << '\n'; 
-    std::cout << "i don't ike pizaa" ;
+    std::cout << "i like pizaa" ; 
+    std::cout << "i don't ike pizaa" << '\n' ;
+
+    int x = 10;
+    int y=10;
+    int sum= x+y+x;
+    std::cout << x <<'\n';
+    std::cout<< sum;
 
     return 0;
 } 
