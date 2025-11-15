96fb8fe45fd048fbdb8d5aa067845ae77c25ebab
manojgowdamj01-cmd <manojgowdamj01@gmail.com>
2025-11-15 10:42:27 +0530

    day2

--- day2.cpp
+++ day2.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
-
+using namespace std;
 int main(){
-    std::cout<< "hii";
+    int a=12;
+    int b=13;
+     float c=30.33;
+
+    cout<< "the value of a is "<<a <<" \nthe value of b is "<<b<<"\nthe value of float "<<c;
+    cout<< "the value of a is "<<a <<" \nthe value of b is "<<b<<"\nthe value of float "<<c;
+    cout<< "the value of a is "<<a <<" \nthe value of b is "<<b<<"\nthe value of float "<<c;
+    return 0;
 }
---
+++ day2b.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1,num2;
+    cout<< "the value of num1:\n";
+    cin>>num1;
+    cout<<"the value of number "<<num1;
+
+}
