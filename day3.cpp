a17f125fb4b9d43299626c03c8dae84687e9f3cf
manojgowdamj01-cmd <manojgowdamj01@gmail.com>
2025-11-24 22:52:02 +0530

    constant

---
+++ day3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+// this is ingnored
+/*
+ssww
+wdwd
+*/
+
+int main()
+{
+
+    bool student = true;
+
+    int age = 23;
+
+
+    string name = "manoj";
+    cout<< "hello "<< name << '\n';
+    cout<< "your age is "<< age  << '\n';
+    cout<<boolalpha;
+
+    return 0;
+}
---
+++ day3b.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    double lenght = 10;
+    double breadth = 20;
+    double area =  0.5*lenght * breadth;
+    cout<< "your area of given triangle is " << area << '\n';
+    return 0;
+
+}
